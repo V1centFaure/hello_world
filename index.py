2c4c241a21a2d6ec666314ba616658b11fac041e
Vincent <vincent.faure@le-campus-numerique.fr>
2022-01-14 11:13:14 +0100

    add a new image

--- index.py
+++ index.py
@@ -1,9 +1,11 @@
 from style import Style
 from image import Image
+from image2 import Image2
 # Premier fichier partagé sur GIT
 
 print("Hello world")
 a: Style = ""
 b: Image = ""
+c: Image2 = ""
 print("test")
 print("test2")
